b52cc0ef37e0814989b12fa272a109fc7270bf27
seonghoo1217 <lsh6451217@gmail.com>
2024-04-24 01:22:39 +0900

    #9 refactor: remove caching number

--- proxy.c
+++ proxy.c
@@ -248,7 +248,6 @@ void parse_uri(char *uri, char *hostname, char *path, int *port) {
 // Cahce
 
 void cache_init() {
-    cache.cache_num = 0; // 맨 처음이니까
     int i;
     for (i = 0; i < CACHE_OBJS_COUNT; i++) {
         cache.cacheobjs[i].LRU = 0; // LRU : 우선 순위를 미는 것. 처음이니까 0
